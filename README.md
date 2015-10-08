31228b170c5e39c1655b4ee603ca0a6e8917a4bb
Noah Paessel <knowuh@gmail.com>
2015-10-08 18:19:58 -0400

    Initial Checkin of fonts and README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # codap-ivy-icons
-Repository of shared icon fonts for Codap and Ivy
+This is a repository for the shared web icon fonts for Codap, and
+the Building Models projects of the [Concord Consortium](http://concord.org/).
+
+
+## TODO
+
+* Instructions for editing & modifiying icon sets from [icomoon](https://icomoon.io)
+* Instructions for using icon sets
+* Instructions for building github pages site
+* Instructions for deploying to S3 and Cloudfront
+* Instructions for using the fonts
---
+++ build.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+DEPLOY_ENV="staging"
+
+if [[ ($# -eq 1 ) && $1 =~ "pro" ]]; then
+  DEPLOY_ENV='production'
+fi
+
+
+read -p "Deploy to $DEPLOY_ENV? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+DISTDIR="./dist"
+GITCONFIG="$DISTDIR/.git/config"
+HERE=`pwd`
+DATE=`date +"%Y-%m-%d"`
+SHA=`git rev-parse --short=8 HEAD`
+
+echo "Building gh-pages branch in $DISTDIR for SHA $SHA on $DATE"
+
+# 1) make sure dist folder variable is not empty and folder exists
+if [ -z "$DISTDIR" ]; then
+  echo "DISTDIR variable is empty, aborting!"
+  exit 1
+fi
+mkdir -p $DISTDIR
+
+# 2) Checkout a new clone of our repo into the dist directory
+# which will contain the gh-pages branch we will deploy
+if [ -e "$GITCONFIG" ]
+then
+  echo "found existing git clone in $DISTDIR"
+  cd $DISTDIR &&\
+  git checkout gh-pages &&\
+  git pull
+else
+  echo "cloning building-models repo in $DISTDIR"
+  rm -rf "$DISTDIR/*" &&\
+  git clone git@github.com:concord-consortium/building-models.git $DISTDIR &&\
+  cd $DISTDIR &&\
+  echo "checking out gh-pages branch" &&\
+  git checkout gh-pages
+fi
+
+# 3) clear out the dist directory
+echo "Clearing out existing files"
+git rm -rf .
+git clean -fxd
+
+cd $HERE
+
+# 4) Build our project using gulp into the dist folder
+echo "Copying Demo Files …"
+echo "$SHA built on $DATE" >> $DISTDIR/buildinfo.txt
+ENVIRONMENT=$DEPLOY_ENV gulp build-all --production --buildInfo '$SHA built on $DATE' &&\
+cp -r ./CodapIvy/* $DISTDIR 
+
+# # 5a) Either Deploy Production to S3,
+# if [[ $DEPLOY_ENV =~ "pro" ]]
+# then
+#   echo
+#   echo Deploying to S3
+#   echo
+#   s3_website push
+#
+# # 5b) Or Deploy Staging to gh-pages
+# else
+#   echo
+#   echo Deploying to gh-page
+#   echo
+#   cd $DISTDIR
+#   git add * &&\
+#   git commit -a -m "deployment for $SHA built on $DATE" &&\
+#   git push origin gh-pages
+# fi
